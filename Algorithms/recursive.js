284552b0d46482dae404cff4dcdd8eb1f19dd893
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-02-25 21:49:00 -0500

    recursive function

---
+++ Algorithms/recursive.js
@@ -0,0 +1,21 @@
+var factorial = function fac(num){
+    if(num===1){
+        return 1;
+    }
+    return num*fac(num-1)
+}
+
+var test = factorial(5)
+console.log(test)
+
+//ES6
+const factorialTest = (n)=>{
+    if (n===1){
+        return 1;
+    }
+    return n * factorialTest(n-1)
+}
+
+
+var answer = factorialTest(5)
+console.log(answer)
