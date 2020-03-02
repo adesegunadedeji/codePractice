670a1c8b57141e5f132598e3a471c9194e63c907
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-03-02 13:45:16 -0500

    permutationsChecker

--- Algorithms/hashtable.js
+++ Algorithms/hashtable.js
@@ -1,19 +1,49 @@
 /* Is Unique: Implement an algorithm to determine if a string has all unique characters. 
 What if you cannot use additional data structures. */
-const string = "ABCDEFE"
+// const string = "ABCDEFE"
 
-const isUnique = (str)=>{
-for (let i= 0; i < str.length; i++){
-    for (let j = i+ 1; j < str.length; j++){
-        if (str[i]=== str[j]){
-            return false
-        }
+// const isUnique = (str)=>{
+// for (let i= 0; i < str.length; i++){
+//     for (let j = i+ 1; j < str.length; j++){
+//         if (str[i]=== str[j]){
+//             return false
+//         }
 
-    }
+//     }
   
-}
-return true
+// }
+// return true
+// }
+
+// console.log(isUnique(string))
+
+
+let string1= "abcd"
+let string2= "dbac"
+
+
+let sort1 = sort(string1);
+let sort2 = sort(string2);
+
+
+
+
+function sort(string){
+    return string.split("").sort().toString()
 }
 
-console.log(isUnique(string))
+const permutationChecker = (str, str2)=>{
+    let sort1 = sort(str);
+    let sort2 = sort(str2);
+    //Check to make sure Lengths of String Equal. and tht boths string are not null
+    if (!str || !str2|| str.length !== str2.length){
+        return false;
+    }
+   
+       
+      else if (sort1===sort2){
+         return true;
+      }
+}
 
+console.log(permutationChecker(string1,string2))
