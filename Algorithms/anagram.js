1bed7a37f9b9d7b71ff8cdef076f32f68426af6b
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-03-30 22:38:41 -0700

    anagram solution added

--- Algorithms/anagram.js
+++ Algorithms/anagram.js
@@ -1,23 +1,21 @@
-function sherlockAndAnagrams(s) { let anagrams = 0;
+// function sherlockAndAnagrams(s) { let anagrams = 0;
 
-    for (let x = 1; x < s.length + 1; x++) {
-        const stringMap = {};
+//     for (let x = 1; x < s.length + 1; x++) {
+//         const stringMap = {};
     
-        for (let y = 0; y < s.length - x + 1; y++) {
-            const string = s.substring(y, y + x).split('').sort().join('');
-            stringMap[string] = (stringMap[string] || 0) + 1;
-        }
+//         for (let y = 0; y < s.length - x + 1; y++) {
+//             const string = s.substring(y, y + x).split('').sort().join('');
+//             stringMap[string] = (stringMap[string] || 0) + 1;
+//         }
     
-        Object.values(stringMap).forEach((value) => {
-            anagrams += value * (value - 1)/2;
-        });
-    }
+//         Object.values(stringMap).forEach((value) => {
+//             anagrams += value * (value - 1)/2;
+//         });
+//     }
     
-    return anagrams;
-    }
-
-
-    console.log(sherlockAndAnagrams("abba"))
+//     return anagrams;
+//     }
+//     console.log(sherlockAndAnagrams("abba"))
 
 // function getAllSubstrings(str) {
 //     let i, j, result = [];
@@ -33,3 +31,56 @@ function sherlockAndAnagrams(s) { let anagrams = 0;
 
 // let string = "abba"
 //   console.log(getAllSubstrings(string))
+
+function anagramHacker(s){
+    let result =1 , s1, s2;
+        //Continue with Code
+        if (s.length % 2 == 0){
+            s1 = s.substring(s, s.length/2);
+            console.log(s1);
+            s2 = s.substring(s.length/2)
+            console.log(s2);
+
+            for (let i =0; i <s1.length; i++){
+                //Check to make sure character is present in h2;
+                if (s2.indexOf(s1[i]) !== -1){
+                    s2 = s2.replace(s1[i], '')
+                }
+                return s2.length;
+            }
+        }
+        else{
+            return result;
+        }
+}
+
+anagramHacker('abccde');
+
+function anagram(s) {
+    let result = -1 
+        //Continue with Code
+        if (s.length % 2 != 0){
+         return result;
+        }
+
+        //Divide the input string into two halves.
+           let s1 = s.substring(s, s.length/2);
+            console.log(s1);
+           let s2 = s.substring(s.length/2)
+            console.log(s2);
+
+            for (let i =0; i <s1.length; i++){
+                //Check to make sure character is present in h2;
+                //Count all common character in the halves.
+                //The result is length of the second half after deletions.
+
+                if (s2.indexOf[s1[i]] !== -1){
+                    //replace exisiting characeter with space
+                    s2 = s2.replace(s1[i], '')
+                }
+    
+            }
+            return s2.length;
+}
+
+console.log(anagramHacker("aaabbb"))
